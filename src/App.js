78ceb5924538c50f2ff50f3757a512275ac11aa9
Bronwyn Harris <bronwyneharris@gmail.com>
2019-10-04 16:51:04 -0400

    create graphQL chat room

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+// Import GraphQL helpers
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import * as compose from 'lodash.flowright';
+
+// App component styles
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Chatbox UI Component
+import Chatbox from './components/Chatbox';
+
+class App extends Component {
+  state = {
+    from: 'Bronwyn',
+    content: ''
+  };
+  componentDidMount() {
+    const from = window.prompt('username');
+    from && this.setState({ from });
+    this._subscribeToNewChats();
+  }
+  _createChat = async e => {
+    if (e.key === 'Enter') {
+      const { content, from } = this.state;
+      await this.props.createChatMutation({
+        variables: { content, from }
+      });
+      this.setState({ content: '' });
+    }
+  };
+  _subscribeToNewChats = () => {
+    this.props.allChatsQuery.subscribeToMore({
+        document: gql`
+          subscription {
+            Chat(filter: { mutation_in: [CREATED] }) {
+              node {
+                id
+                from
+                content
+                createdAt
+              }
+            }
+          }
+        `,
+        updateQuery: (previous, { subscriptionData }) => {
+          const newChatLinks = [
+            ...previous.allChats,
+            subscriptionData.data.Chat.node
+          ];
+          const result = {
+            ...previous,
+            allChats: newChatLinks
+          };
+          return result;
+        }
+      });
+    };
+  render() {
+    const allChats = this.props.allChatsQuery.allChats || [];
+    return (
+      <div className="">
+        <div className="container">
+          <h2>Chats</h2>
+          {allChats.map(message => (
+            <Chatbox key={message.id} message={message} />
+          ))}
+          <input
+            value={this.state.content}
+            onChange={e => this.setState({ content: e.target.value })}
+            type="text"
+            placeholder="Start typing"
+            onKeyPress={this._createChat}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+const ALL_CHATS_QUERY = gql`
+  query AllChatsQuery {
+    allChats {
+      id
+      createdAt
+      from
+      content
+    }
+  }
+`;
+
+const CREATE_CHAT_MUTATION = gql`
+mutation CreateChatMutation($content: String!, $from: String!) {
+  createChat(content: $content, from: $from) {
+    id
+    createdAt
+    from
+    content
+  }
+}
+`;
+
+export default compose(
+  graphql(ALL_CHATS_QUERY, { name: 'allChatsQuery' }),
+  graphql(CREATE_CHAT_MUTATION, { name: 'createChatMutation'})
+  )(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink, split } from 'apollo-client-preset'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const wsLink = new WebSocketLink({
+  uri: 'wss://subscriptions.ap-northeast-1.graph.cool/v1/ck1axsye127gp0109mv821d0e',
+  options: {
+      reconnect: true
+  }
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/ck1axsye127gp0109mv821d0e' })
+
+const link = split(
+  ({ query }) => {
+      const { kind, operation } = getMainDefinition(query)
+      return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink,
+)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
